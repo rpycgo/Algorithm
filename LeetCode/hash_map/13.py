282bc8f4f0717d53b0a524bec51def197e3d5349
rpycgo <skyd0608@gmail.com>
2025-08-09 01:41:43 +0900

    refactor: 13.py

--- LeetCode/hash_map/13.py
+++ LeetCode/hash_map/13.py
@@ -1,18 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         total = 0
-        previous_character = ''
+        previous_value = 0
         mapping = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-        for symbol in s:
-            if previous_character == 'I' and symbol in ('V', 'X'):
-                total -= mapping.get(previous_character)*2
-            elif previous_character == 'X' and symbol in ('L', 'C'):
-                total -= mapping.get(previous_character)*2
-            elif previous_character == 'C' and symbol in ('D', 'M'):
-                total -= mapping.get(previous_character)*2
+        for symbol in reversed(s):
+            value = mapping.get(symbol)
 
-            total += mapping.get(symbol)
-            previous_character = symbol
+            if value < previous_value:
+                total -= value
+            else:
+                total += value
+
+            previous_value = value
 
         return total
