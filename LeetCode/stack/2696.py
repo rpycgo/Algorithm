418130b7bd687b13f18a25fb7ac1e073ef743488
rpycgo <skyd0608@gmail.com>
2025-10-18 20:53:20 -0400

    refactor: Modify 2696.py

--- LeetCode/stack/2696.py
+++ LeetCode/stack/2696.py
@@ -2,12 +2,13 @@ class Solution:
     def minLength(self, s: str) -> int:
         stack = []
         for char in s:
+            if stack:
+                top = stack[-1]
+
+                if (top == 'A' and char == 'B') or (top == 'C' and char == 'D'):
+                    stack.pop()
+                    continue
+
             stack.append(char)
 
-            while len(stack) >= 2 and (''.join(stack[-2:]) == 'AB' or ''.join(stack[-2:]) == 'CD'):            
-                stack.pop()
-                stack.pop()
-
-        s = ''.join(stack)
-
-        return len(s)
+        return len(stack)
