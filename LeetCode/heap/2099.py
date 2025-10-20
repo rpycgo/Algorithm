2b1c14daa89a9c1fab904d91e96835fb8d0acf6d
rpycgo <skyd0608@gmail.com>
2025-10-19 20:20:20 -0400

    refactor: Modify 2099.py

--- LeetCode/heap/2099.py
+++ LeetCode/heap/2099.py
@@ -1,17 +1,12 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        original_nums = nums.copy()
-        nums = [(-num, i) for i, num in enumerate(nums)]
-        heapq.heapify(nums)
+        heap = [(-num, i) for i, num in enumerate(nums)]
+        heapq.heapify(heap)
+
         top_k_index = []
-        top_k = []
-
-        while len(top_k_index) != k:
-            _, i = heapq.heappop(nums)
-
-            top_k_index.append(i)
+        for _ in range(k):
+            top_k_index.append(heapq.heappop(heap)[1])
 
         top_k_index.sort()
-        top_k = [original_nums[i] for i in top_k_index]
 
-        return top_k
+        return [nums[i] for i in top_k_index]
