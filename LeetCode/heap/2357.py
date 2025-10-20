dd53508de767123ec0140643e5f80af763f25e0f
rpycgo <skyd0608@gmail.com>
2025-10-20 16:54:03 -0400

    refactor: Modify 2357.py

--- LeetCode/heap/2357.py
+++ LeetCode/heap/2357.py
@@ -1,14 +1,17 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         heap = [num for num in nums if num != 0]
+
         heapq.heapify(heap)
-
         count = 0
-        while sum(heap):
-            pop_value = heapq.heappop(heap)
-            heap = [num - pop_value for num in heap if num - pop_value != 0]
-            heapq.heapify(heap)
+        previous = 0
 
-            count += 1
-        
+        while heap:
+            pop_value = heapq.heappop(heap)
+
+            if pop_value > previous:
+                previous = pop_value
+
+                count += 1
+
         return count
