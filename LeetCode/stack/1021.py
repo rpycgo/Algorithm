9baf38830555052d8572fdeb7ad3e8b40cf7e547
rpycgo <skyd0608@gmail.com>
2025-10-22 16:40:08 -0400

    Upload 1021.py

---
+++ LeetCode/stack/1021.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        stack = []
+        result = []
+
+        for char in s:
+            if char == '(':
+                if stack:
+                    result.append(char)
+
+                stack.append(char)
+            else:
+                stack.pop()
+
+                if stack:
+                    result.append(char)
+
+        return ''.join(result)
