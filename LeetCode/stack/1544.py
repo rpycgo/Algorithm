6dddaf3a487cd9e4cbf6ab9d6089d57f33a7a253
rpycgo <skyd0608@gmail.com>
2025-10-22 16:29:06 -0400

    refactor: Modifies 1544.py

--- LeetCode/stack/1544.py
+++ LeetCode/stack/1544.py
@@ -1,16 +1,11 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        if len(s) == 1:
-            return s
+        stack = []
 
-        stack = [s[0]]
-        i = 1
-        while i != len(s):
-            if stack and stack[-1] != s[i] and (stack[-1] == s[i].lower() or stack[-1] == s[i].upper()):
+        for char in s:
+            if stack and abs(ord(stack[-1]) - ord(char)) == 32:
                 stack.pop()
             else:
-                stack.append(s[i])
-
-            i += 1
+                stack.append(char)
 
         return ''.join(stack)
