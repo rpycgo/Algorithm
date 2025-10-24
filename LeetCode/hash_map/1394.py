7fbb6083c84ab944569e45bb181397d726d8778d
rpycgo <skyd0608@gmail.com>
2025-10-24 15:48:42 -0400

    refactor: Modifies 1394.py

--- LeetCode/hash_map/1394.py
+++ LeetCode/hash_map/1394.py
@@ -4,15 +4,13 @@ from collections import defaultdict
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
         count = defaultdict(int)
-        candidates = []
+        answer = -1
 
         for num in arr:
             count[num] += 1
 
-        for _, num in enumerate(arr):
-            if count[num] == num:
-                candidates.append(num)
-
-        answer = max(candidates) if candidates else -1
+        for num, freq in count.items():
+            if num == freq:
+                answer = max(answer, num)
 
         return answer
