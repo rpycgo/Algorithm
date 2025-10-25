f5f653db79839cdc2d156f50f6e1be044ca6d3c0
rpycgo <skyd0608@gmail.com>
2025-10-25 19:35:24 -0400

    Upload 2309.py

--- LeetCode/hash_map/2309.py
+++ LeetCode/hash_map/2309.py
@@ -1,19 +1,8 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
-        lower_count = defaultdict(int)
-        upper_count = defaultdict(int)
+        chars = set(s)
+        for char in reversed('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+            if char in chars and char.lower() in chars:
+                return char
 
-        for char in s:
-            if 'a' <= char <= 'z':
-                lower_count[char] += 1
-            else:
-                upper_count[char] += 1
-
-        answer = []
-        for i in range(97, 123):
-            if lower_count[chr(i)] >= 1 and upper_count[chr(i-32)] >= 1:
-                answer.append(chr(i-32))
-
-        answer = max(answer) if answer else ''
-
-        return answer
+        return ''
