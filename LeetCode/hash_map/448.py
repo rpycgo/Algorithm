97864f33ed211edece23df7a3ff198930f3ccd07
rpycgo <skyd0608@gmail.com>
2025-10-30 13:26:10 -0400

    refactor: Modifies 448.py

--- LeetCode/hash_map/448.py
+++ LeetCode/hash_map/448.py
@@ -1,14 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        count = defaultdict(int)
-
-        n = len(nums)
-        for num in range(1, n+1):
-            count[num] += 1
-
         for num in nums:
-            count[num] -= 1
+            index = abs(num) - 1
 
-        answer = [key for key, value in count.items() if value == 1]
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+
+        answer = [i + 1 for i, num in enumerate(nums) if num > 0]
 
         return answer
