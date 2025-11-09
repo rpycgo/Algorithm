bd22903b82dcd8d955bd01fd26e6714aab5c53c5
rpycgo <skyd0608@gmail.com>
2025-11-09 18:49:35 -0500

    refactor: Modifies 786.py using lazy generation

--- LeetCode/heap/786.py
+++ LeetCode/heap/786.py
@@ -1,13 +1,17 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        n = len(arr)
         heap = []
-        for i in range(len(arr)-1):
-            for j in range(1, len(arr)):
-                heapq.heappush(heap, (-arr[i]/arr[j], arr[i], arr[j]))
 
-                if len(heap) > k:
-                    heapq.heappop(heap)
+        for i in range(n - 1):
+            heapq.heappush(heap, (arr[i]/arr[-1], i, n-1))
 
-        answer = heap[0][1:]
+        for _ in range(k - 1):
+            _, i, j = heapq.heappop(heap)
+            if j - 1 > i:
+                heapq.heappush(heap, (arr[i]/arr[j - 1], i, j-1))
+
+        _, i, j = heap[0]
+        answer = [arr[i], arr[j]]
 
         return answer
