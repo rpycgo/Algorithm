1c7048cb1af17c3ca2608d7722b28041d764e25f
rpycgo <skyd0608@gmail.com>
2025-11-11 22:17:29 -0500

    refactor: Modifies 183.sql using NOT EXISTS

--- LeetCode/database/183.sql
+++ LeetCode/database/183.sql
@@ -1,10 +1,10 @@
 SELECT
-    a.name as Customers
+    name AS Customers
 FROM
     Customers a
-LEFT JOIN
-    Orders b
-ON
-    a.id = b.customerId
 WHERE
-    b.customerId IS NULL
+    NOT EXISTS (
+        SELECT 1
+        FROM Orders b
+        WHERE b.customerId = a.id
+    );
