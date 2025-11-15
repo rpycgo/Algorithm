10ad389e9bb0867166d1c1caa843b3542b2be1c9
rpycgo <skyd0608@gmail.com>
2025-11-14 23:39:38 -0500

    refactor: Modifies 120.py by deleting edge case

--- LeetCode/dynamic_programming/120.py
+++ LeetCode/dynamic_programming/120.py
@@ -2,9 +2,6 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
 
-        if n == 1:
-            return triangle[0][0]
-
         for i in range(1, n):
             for j in range(i+1):
                 if j == 0:
