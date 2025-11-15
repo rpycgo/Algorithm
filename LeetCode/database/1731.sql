738e43483ecc2e08483ec835b8f30793d35ec1c5
rpycgo <skyd0608@gmail.com>
2025-11-14 22:38:56 -0500

    Upload 1731.sql

---
+++ LeetCode/database/1731.sql
@@ -0,0 +1,16 @@
+SELECT
+    e1.employee_id, e1.name,
+    COUNT(e2.employee_id) AS reports_count,
+    ROUND(AVG(e2.age), 0) AS average_age
+FROM
+    Employees e1
+LEFT JOIN
+    Employees e2
+ON
+    e1.employee_id = e2.reports_to
+GROUP BY
+    e1.employee_id
+HAVING
+    COUNT(e2.employee_id) > 0
+ORDER BY
+    e1.employee_id
