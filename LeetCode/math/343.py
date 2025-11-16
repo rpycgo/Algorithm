d124fa709a516ae80364a82cefd7f920b84e668e
rpycgo <skyd0608@gmail.com>
2025-11-15 19:12:46 -0500

    Upload 343.py

---
+++ LeetCode/math/343.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        result = 1
+        while n > 4:
+            result *= 3
+
+            n -= 3
+
+        answer = result * n
+
+        return answer
