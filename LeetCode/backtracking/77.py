d8701f91a56b9ccbfbe55f4535e0647d08707a85
rpycgo <skyd0608@gmail.com>
2025-11-15 20:26:56 -0500

    Upload 77.py

---
+++ LeetCode/backtracking/77.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        candidates = range(1, n + 1)
+        answer = []
+
+        self.backtrack(answer, candidates, 0, [], k)
+
+        return answer
+
+    def backtrack(self, answer, candidates, start, path, k):
+        if len(path) == k:
+            answer.append(path[:])
+            return
+
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i-1]:
+                continue
+
+            path.append(candidates[i])
+
+            self.backtrack(answer, candidates[i+1:], start, path, k)
+
+            path.pop()
