100d69df3150b95404fbee855df32030a87bd0b9
rpycgo <skyd0608@gmail.com>
2025-12-15 23:09:47 -0500

    refactor: Modifies 219.py by saving last index

--- LeetCode/hash_map/219.py
+++ LeetCode/hash_map/219.py
@@ -1,22 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hash_map = defaultdict(list)
+        last_index = {}
 
         for i, num in enumerate(nums):
-            hash_map[num].append(i)
-
-        hash_map_w_least_2_values = {
-            key: value
-            for key, value
-            in hash_map.items()
-            if len(value) >= 2
-        }
-
-        for value in hash_map_w_least_2_values.values():
-            n = len(value)
-            for i in range(n):
-                for j in range(i+1, n):
-                    if abs(value[i]-value[j]) <= k:
-                        return True
+            if (num in last_index) and (i-last_index[num] <= k):
+                return True
+            last_index[num] = i
 
         return False
