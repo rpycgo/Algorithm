8556a6bd55f6ffc3343a66d4b1fc2e22628791cb
rpycgo <skyd0608@gmail.com>
2025-12-15 22:46:43 -0500

    refactor: Modifies 187.py using bit mask

--- LeetCode/hash_map/187.py
+++ LeetCode/hash_map/187.py
@@ -1,10 +1,20 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        dnas = defaultdict(int)
+        encode = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
 
-        for i in range(len(s)-10+1):
-            dnas[s[i:i+10]] += 1
+        seen = set()
+        repeated = set()
 
-        answer = [key for key, value in dnas.items() if value >= 2]
+        mask = 0
+        for i, char in enumerate(s):
+            mask = ((mask << 2) | encode[char]) & ((1 << 20) - 1)
+
+            if i >= 9:
+                if mask in seen:
+                    repeated.add(s[i-9:i+1])
+                else:
+                    seen.add(mask)
+
+        answer = list(repeated)
 
         return answer
