a61a4c6015713c3aad217776f7abe9a15c6b9222
rpycgo <skyd0608@gmail.com>
2025-12-16 22:18:25 -0500

    refactor: Modifies 1534.py by deleting unnecessary tuple save

--- LeetCode/enumeration/1534.py
+++ LeetCode/enumeration/1534.py
@@ -2,13 +2,11 @@ class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
         n = len(arr)
 
-        good_triplets = []
+        answer = 0
         for i in range(n-2):
             for j in range(i+1, n-1):
                 for k in range(j+1, n):
                     if (abs(arr[i]-arr[j]) <= a) and (abs(arr[j]-arr[k]) <= b) and (abs(arr[i]-arr[k]) <= c):
-                        good_triplets.append((arr[i], arr[j], arr[k]))
-
-        answer = len(good_triplets)
+                        answer += 1
 
         return answer
