246cd58c1ccc78221b5abb274b8b07bff78b9e7c
rpycgo <skyd0608@gmail.com>
2025-12-18 21:33:30 -0500

    refactor: Modifies 2574.py

--- LeetCode/prefix_sum/2574.py
+++ LeetCode/prefix_sum/2574.py
@@ -1,18 +1,14 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        total = sum(nums)
+        left = 0
+        answer = []
 
-        left_sum = nums[:]
-        for i in range(1, n-1):
-            left_sum[i] += left_sum[i-1]
+        for num in nums:
+            right = total - left - num
 
-        left_sum = [0] + left_sum[:-1]
+            answer.append(abs(left - right))
 
-        right_sum = nums[:]
-        right_sum[0] = sum(nums) - nums[0]
-        for i in range(1, n):
-            right_sum[i] = right_sum[i-1]  - right_sum[i]
-
-        answer = [abs(left - right) for left, right in zip(left_sum, right_sum)]
+            left += num
 
         return answer
