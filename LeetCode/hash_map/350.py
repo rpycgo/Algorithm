7cb87540129eb15025dd93c8f8309da4980d6f07
rpycgo <skyd0608@gmail.com>
2025-12-28 21:45:16 -0500

    refactor: Modifies 350.py using only one hash table

--- LeetCode/hash_map/350.py
+++ LeetCode/hash_map/350.py
@@ -1,18 +1,15 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hash_map1 = defaultdict(int)
-        hash_map2 = defaultdict(int)
+        count = defaultdict(int)
 
         for num in nums1:
-            hash_map1[num] += 1
-        for num in nums2:
-            hash_map2[num] += 1
-
-        common_keys = hash_map1.keys() & hash_map2.keys()
-        min_values_by_key = [min(hash_map1[key], hash_map2[key]) for key in common_keys]
+            count[num] += 1
 
         answer = []
-        for common_key, min_value_by_key in zip(common_keys, min_values_by_key):
-            answer.extend([common_key] * min_value_by_key)
+        for num in nums2:
+            if count[num] > 0:
+                answer.append(num)
+
+                count[num] -= 1
 
         return answer
