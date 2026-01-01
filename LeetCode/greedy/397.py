70e67445270c145a622cf17b5a540651c92f81b4
rpycgo <skyd0608@gmail.com>
2025-12-31 21:21:16 -0500

    Upload 397.py

---
+++ LeetCode/greedy/397.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        count = 0
+        while n != 1:
+            count += 1
+
+            if n%2 == 0:
+                n //= 2
+            else:
+                if n == 3 or ((n >> 1) & 1) == 0:
+                    n -= 1
+                else:
+                    n += 1
+
+        return count
