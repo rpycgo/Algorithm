deffdf433847c5e21365bcac1aac39ae05c6dc62
rpycgo <skyd0608@gmail.com>
2026-01-07 15:20:25 -0500

    Upload 9655.py

---
+++ BOJ/Python/Dynamic Programming/9655.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+
+    N = int(input())
+
+    dp = [-1] * 1001
+    dp[1] = 1
+    dp[2] = 0
+    dp[3] = 1
+
+    for i in range(4, N+1):
+        if dp[i-1] == 0 or dp[i-3] == 0:
+            dp[i] = 1
+        else:
+            dp[i] = 0
+
+    print('SK' if dp[N] == 1 else 'CY')
+
+
+if __name__ == '__main__':
+    main()
