9f66061b30253ec44a1749b8639e9138e571aafe
rpycgo <skyd0608@gmail.com>
2026-01-07 16:13:56 -0500

    Upload 2747.py

---
+++ BOJ/Python/Dynamic Programming/2747.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input())
+
+    dp = [0] * (n+1)
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    answer = dp[n]
+
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
