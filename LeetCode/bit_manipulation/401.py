36c132eeb7b62ee4d4e09efa38775d40409d996e
rpycgo <skyd0608@gmail.com>
2026-01-09 20:48:43 -0500

    Upload 401.py

---
+++ LeetCode/bit_manipulation/401.py
@@ -0,0 +1,10 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        answer = []
+
+        for hour in range(12):
+            for minute in range(60):
+                if (bin(hour).count('1') + bin(minute).count('1')) == turnedOn:
+                    answer.append(f'{hour}:{minute:02d}')
+
+        return answer
