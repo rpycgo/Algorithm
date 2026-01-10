f574337b413f32e77b86374184138fd3d52b316b
rpycgo <skyd0608@gmail.com>
2026-01-10 15:42:32 -0500

    Upload 28278.py

---
+++ BOJ/Python/Stack/28278.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+
+    stack = []
+
+    N = int(input())
+    for _ in range(N):
+        command = list(map(int, input().split()))
+
+        if command[0] == 1:
+            stack.append(command[1])
+        elif command[0] == 2:
+            print(stack.pop() if stack else -1)
+        elif command[0] == 3:
+            print(len(stack))
+        elif command[0] == 4:
+            print(0 if stack else 1)
+        elif command[0] == 5:
+            print(stack[-1] if stack else -1)
+
+
+if __name__ == '__main__':
+    main()
