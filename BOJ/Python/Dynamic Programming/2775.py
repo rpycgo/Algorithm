4614313405fb3cffeb0d2d3fa1df078630ace337
rpycgo <skyd0608@gmail.com>
2026-01-09 20:25:03 -0500

    Upload 2775.py

---
+++ BOJ/Python/Dynamic Programming/2775.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+
+    T = int(input())
+
+    for _ in range(T):
+        k = int(input())
+        n = int(input())
+
+        people = [i for i in range(1, n+1)]
+
+        for _ in range(k):
+            for j in range(1, n):
+                people[j] += people[j-1]
+
+        answer = people[-1]
+
+        print(answer)
+
+
+if __name__ == '__main__':
+    main()
