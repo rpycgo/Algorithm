2ccaa1d8d78f7038831ae8a622e9639b779cbeec
rpycgo <skyd0608@gmail.com>
2026-01-10 20:04:17 -0500

    refactor: Updates 13397.py using range

--- BOJ/Python/Binary Search/13397.py
+++ BOJ/Python/Binary Search/13397.py
@@ -1,20 +1,20 @@
 import sys
 
 
-def calculate_n_intervals(limit, numbers):
+def calculate_n_intervals(limit, N, numbers):
     count = 1
     min_val = numbers[0]
     max_val = numbers[0]
 
-    for number in numbers[1:]:
-        min_val = min(min_val, number)
-        max_val = max(max_val, number)
+    for i in range(1, N):
+        min_val = min(min_val, numbers[i])
+        max_val = max(max_val, numbers[i])
 
         if max_val - min_val > limit:
             count += 1
 
-            min_val = number
-            max_val = number
+            min_val = numbers[i]
+            max_val = numbers[i]
 
     return count
 
@@ -32,11 +32,11 @@ def main():
     while left <= right:
         mid = (left+right) // 2
 
-        if calculate_n_intervals(mid, numbers) <= M:
+        if calculate_n_intervals(mid, N, numbers) <= M:
             answer = mid
             right = mid - 1
         else:
-            left = mid - 1
+            left = mid + 1
 
     print(answer)
 
