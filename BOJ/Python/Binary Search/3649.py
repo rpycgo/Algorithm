a007b692e324c2f12a9a81b8c0fe7a236ddafcd6
rpycgo <skyd0608@gmail.com>
2026-01-10 23:39:03 -0500

    refactor: Modifies 3649.py using generator

--- BOJ/Python/Binary Search/3649.py
+++ BOJ/Python/Binary Search/3649.py
@@ -2,35 +2,46 @@ import sys
 from bisect import bisect_left
 
 
+def get_input():
+    for line in sys.stdin:
+        for word in line.split():
+            yield word
+
+
 def main():
-    input = sys.stdin.read
-    input_data = input().split()
+    input_data = get_input()
 
-    ptr = 0
-    while ptr < len(input_data):
-        x_nm = int(input_data[ptr]*10000000)
-        ptr += 1
+    while True:
+        try:
+            x_str = next(input_data)
+            x_nm = int(x_str)*10_000_000
 
-        n = int(input_data[ptr])
-        ptr += 1
+            n_str = next(input_data)
+            n = int(n_str)
 
-        lengths = []
-        for _ in range(n):
-            lengths.append(int(input_data[ptr]))
-            ptr += 1
-        lengths.sort()
+            lengths = []
+            for _ in range(n):
+                length_str = next(input_data)
+                length = int(length_str)
+                lengths.append(length)
+            lengths.sort()
 
-        for length in lengths:
-            target = x_nm-length
-            idx = bisect_left(lengths, target)
+            is_found = False
+            for i, length in enumerate(lengths):
+                target = x_nm - length
 
-            if idx < n and lengths[idx] == target:
-                print(f'yes {length} {target}')
-                break
+                idx = bisect_left(lengths, target, i+1)
 
-        print('danger')
+                if idx < n and lengths[idx] == target:
+                    print(f'yes {length} {target}')
+                    is_found = True
+                    break
+
+            if not is_found:
+                print('danger')
+        except:
+            break
 
 
 if __name__ == '__main__':
     main()
-    
