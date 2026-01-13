ea5f50493b807fb48087d26c10b75599cc62eae7
rpycgo <skyd0608@gmail.com>
2026-01-13 15:44:13 -0500

    Upload 17299.py

---
+++ BOJ/Python/Stack/17299.py
@@ -0,0 +1,27 @@
+import sys
+from collections import Counter
+
+
+def main():
+    input = sys.stdin.readline
+
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    count = Counter(A)
+
+    answer = [-1] * N
+    stack = []
+
+    for i, num in enumerate(N):
+        while stack and count[A[stack[-1]]] < count[num]:
+            idx = stack.pop()
+            answer[idx] = num
+
+        stack.append(i)
+
+    print(*answer)
+
+
+if __name__ == '__main__':
+    main()
