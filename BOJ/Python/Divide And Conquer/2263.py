d3a71662741759433c4b8048fa8d25565a345ac3
rpycgo <skyd0608@gmail.com>
2026-01-14 13:21:49 -0500

    Upload 2263.py

---
+++ BOJ/Python/Divide And Conquer/2263.py
@@ -0,0 +1,39 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+class PostOrder:
+    def __init__(self, n, inorder, postorder):
+        self.n = n
+        self.inorder = inorder
+        self.postorder = postorder
+
+        self.position = [0] * (n+1)
+        for i in range(n):
+            self.position[self.inorder[i]] = i
+
+    def get_preorder(self, in_start, in_end, post_start, post_end):
+        if in_start > in_end or post_start > post_end:
+            return
+
+        root = self.postorder[post_end]
+        print(root, end=' ')
+
+        root_idx = self.position[root]
+        left_count = root_idx - in_start
+
+        self.get_preorder(in_start, root_idx-1, post_start, post_start+left_count-1)
+        self.get_preorder(root_idx+1, in_end, post_start+left_count, post_end-1)
+
+    def run(self):
+        self.get_preorder(0, self.n-1, 0, self.n-1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    inorder = list(map(int, input().split()))
+    postorder = list(map(int, input().split()))
+
+    PostOrder(n, inorder, postorder).run()
