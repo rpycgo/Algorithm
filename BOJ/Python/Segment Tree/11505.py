8231c540ff12f8c60f0295715bc9371871d13760
rpycgo <skyd0608@gmail.com>
2026-01-16 16:31:06 -0500

    Upload 11505.py

---
+++ BOJ/Python/Segment Tree/11505.py
@@ -0,0 +1,74 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+class SegmentTree:
+    def __init__(self, N, arr):
+        self.N = N
+        self.arr = arr
+
+        self.MOD = 1_000_000_007
+
+        self.tree = [0] * (N * 4)
+        self.build(1, 0, N-1)
+
+    def build(self, node, start, end):
+        if start == end:
+            self.tree[node] = self.arr[start]
+            return self.tree[node]
+
+        mid = (start + end) // 2
+        left_val = self.build(node*2, start, mid)
+        right_val = self.build(node*2 + 1, mid+1, end)
+
+        self.tree[node] = left_val * right_val % self.MOD
+
+        return self.tree[node]
+
+    def query(self, node, start, end, left, right):
+        if right < start or end < left:
+            return 1
+
+        if left <= start and end <= right:
+            return self.tree[node]
+
+        mid = (start + end) // 2
+        left_val = self.query(node*2, start, mid, left, right)
+        right_val = self.query(node*2 + 1, mid+1, end, left, right)
+
+        return left_val * right_val % self.MOD
+
+    def update(self, node, start, end, target_idx, new_val):
+        if target_idx < start or target_idx > end:
+            return self.tree[node]
+
+        if start == end:
+            self.tree[node] = new_val
+            return self.tree[node]
+
+        mid = (start + end) // 2
+        left_val = self.update(node*2, start, mid, target_idx, new_val)
+        right_val = self.update(node*2 + 1, mid+1, end, target_idx, new_val)
+
+        self.tree[node] = left_val * right_val % self.MOD
+
+        return self.tree[node]
+
+
+if __name__ == '__main__':
+    N, M, K = map(int, input().split())
+    nums = [int(input()) for _ in range(N)]
+
+    segment_tree = SegmentTree(N, nums)
+
+    for _ in range(M + K):
+        a, b, c = map(int, input().split())
+
+        if a == 1:
+            segment_tree.update(1, 0, N-1, b-1, c)
+        else:
+            result = segment_tree.query(1, 0, N-1, b-1, c-1)
+
+            print(result)
