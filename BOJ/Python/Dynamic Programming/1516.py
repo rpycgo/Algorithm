635f4aff5d0315f783ca11675c4cd5d0c26e64f8
rpycgo <skyd0608@gmail.com>
2026-01-20 20:07:30 -0500

    Upload 1516.py

---
+++ BOJ/Python/Dynamic Programming/1516.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+
+
+input = sys.stdin.readline
+
+
+def main():
+    N = int(input())
+
+    times = [0] * (N+1)
+    indegrees = [0] * (N+1)
+    results = [0] * (N+1)
+
+    adj = [[] for _ in range(N+1)]
+    for i in range(1, N+1):
+        data = tuple(map(int, input().split()))
+        times[i] = data[0]
+
+        for pre in data[1:-1]:
+            adj[pre].append(i)
+            indegrees[i] += 1
+
+    queue = deque()
+    for i in range(1, N+1):
+        if indegrees[i] == 0:
+            queue.append(i)
+            results[i] = times[i]
+
+    while queue:
+        curr_node = queue.popleft()
+
+        for next_node in adj[curr_node]:
+            indegrees[next_node] -= 1
+
+            results[next_node] = max(results[next_node], results[curr_node]+times[next_node])
+
+            if indegrees[next_node] == 0:
+                queue.append(next_node)
+
+    for i in range(1, N+1):
+        print(results[i])
+
+
+if __name__ == '__main__':
+    main()
