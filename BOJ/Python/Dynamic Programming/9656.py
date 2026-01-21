facf48307e158df165468d4e020f120343954df5
rpycgo <skyd0608@gmail.com>
2026-01-20 20:40:29 -0500

    Upload 9565.py

---
+++ BOJ/Python/Dynamic Programming/9656.py
@@ -0,0 +1,27 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+def main():
+    N = int(input())
+
+    dp = [0] * (N+1)
+
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 0
+
+    for i in range(4, N+1):
+        if dp[i-1] == 0 or dp[i-3] == 0:
+            dp[i] = 1
+        else:
+            dp[i] = 0
+
+    answer = 'SK' if dp[N] == 1 else 'CY'
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
