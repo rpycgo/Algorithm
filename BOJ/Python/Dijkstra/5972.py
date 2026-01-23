46f0955947c172d77ac547c0987beba2fa15e54a
rpycgo <skyd0608@gmail.com>
2026-01-23 14:45:01 -0500

    Upload 5972.py

---
+++ BOJ/Python/Dijkstra/5972.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+
+input = sys.stdin.readline
+
+
+def dijkstra(start_node, N, adj):
+    dists = [float('inf')] * (N+1)
+    dists[start_node] = 0
+
+    heap = [(0, start_node)]
+    while heap:
+        curr_cost, curr_node = heapq.heappop(heap)
+
+        if dists[curr_node] < curr_cost:
+            continue
+
+        for next_node, next_weight in adj[curr_node]:
+            new_cost = curr_cost + next_weight
+
+            if new_cost < dists[next_node]:
+                dists[next_node] = new_cost
+                heapq.heappush(heap, (new_cost, next_node))
+
+    return dists
+
+
+def main():
+    N, M = map(int, input().split())
+
+    adj = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+
+        adj[a].append((b, c))
+        adj[b].append((a, c))
+
+    dists = dijkstra(1, N, adj)
+    print(dists[N])
+
+
+if __name__ == '__main__':
+    main()
