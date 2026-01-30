674da9b21a8750190b9e302a32a264bf40c364a6
rpycgo <skyd0608@gmail.com>
2026-01-29 19:43:52 -0500

    refactor: Modifies 2342.py using 3d dp

--- BOJ/Python/Dynamic Programming/2342.py
+++ BOJ/Python/Dynamic Programming/2342.py
@@ -21,30 +21,36 @@ def main():
         print(0)
         return
 
-    dp = {(0, 0): 0}
+    n = len(seqs) - 1
+    dp = [[[float('inf')] * 5 for _ in range(5)] for _ in range(n+1)]
 
-    for target in seqs:
-        if target == 0:
-            break
+    dp[0][0][0] = 0
 
-        next_dp = {}
+    for i in range(n):
+        target = seqs[i]
 
-        for (left, right), cost in dp.items():
-            if target != right:
-                new_cost = cost + get_cost(left, target)
+        for left in range(5):
+            for right in range(5):
+                if dp[i][left][right] != float('inf'):
+                    curr_cost = dp[i][left][right]
 
-                if ((target, right) not in next_dp) or (new_cost < next_dp[(target, right)]):
-                    next_dp[(target, right)] = new_cost
+                    if target != right:
+                        new_cost = curr_cost + get_cost(left, target)
 
-            if target != left:
-                new_cost = cost + get_cost(right, target)
+                        if new_cost < dp[i+1][target][right]:
+                            dp[i+1][target][right] = new_cost
 
-                if ((left, target) not in next_dp) or (new_cost < next_dp[(left, target)]):
-                    next_dp[(left, target)] = new_cost
+                    if target != left:
+                        new_cost = curr_cost + get_cost(right, target)
 
-        dp = next_dp
+                        if new_cost < dp[i+1][left][target]:
+                            dp[i+1][left][target] = new_cost
 
-    answer = min(dp.values())
+    answer = float('inf')
+    for right in range(5):
+        for left in range(5):
+            answer = min(answer, dp[n][left][right])
+
     print(answer)
 
 
